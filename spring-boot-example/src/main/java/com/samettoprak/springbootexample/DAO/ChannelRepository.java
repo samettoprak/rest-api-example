0ae7fc4133886fe0aab73b70acb0bc24f0af8482
samettoprak <topraksamet41@gmail.com>
2022-12-26 15:20:48 +0300

    finding channel by name and owner

--- spring-boot-example/src/main/java/com/samettoprak/springbootexample/DAO/ChannelRepository.java
+++ spring-boot-example/src/main/java/com/samettoprak/springbootexample/DAO/ChannelRepository.java
@@ -1,7 +1,11 @@
 package com.samettoprak.springbootexample.DAO;
 
 import com.samettoprak.springbootexample.Entity.Channel;
+import com.samettoprak.springbootexample.Entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ChannelRepository extends MongoRepository<Channel,String>{
+    List<Channel> findByNameAndOwner(String name, User owner);
 }
