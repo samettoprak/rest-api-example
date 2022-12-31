970512c6e71cc3a60ccf7fbceb1b4ff57e4d93b2
samettoprak <topraksamet41@gmail.com>
2022-12-31 19:47:47 +0300

    Update

--- spring-boot-example/src/main/java/com/samettoprak/springbootexample/SpringBootExampleApplication.java
+++ spring-boot-example/src/main/java/com/samettoprak/springbootexample/SpringBootExampleApplication.java
@@ -1,42 +1,20 @@
 package com.samettoprak.springbootexample;
 
 
-import com.samettoprak.springbootexample.Entity.Message;
-import com.samettoprak.springbootexample.Service.ChannelService;
-import com.samettoprak.springbootexample.Service.LoginService;
-import com.samettoprak.springbootexample.Service.UserService;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @SpringBootApplication
-public class SpringBootExampleApplication  implements CommandLineRunner {
-	private final UserService userService;
-	private final LoginService loginService;
-	private final ChannelService channelService;
-	public SpringBootExampleApplication(UserService userService,LoginService loginService, ChannelService channelService){
-		this.channelService = channelService;
-		this.loginService=loginService;
-		this.userService = userService;
-	}
+public class SpringBootExampleApplication{
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		var user = loginService.CheckLogin("topraksamet41@gmail.com","123");
-		var message = new Message(null, LocalDateTime.now(),"merhaba");
-		var channel = channelService.getAllChannels().get(0);
-		userService.addChannelToUser(user,channel);
 
-
-		}
 
 	}
 
