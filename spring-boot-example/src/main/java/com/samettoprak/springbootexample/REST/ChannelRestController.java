2245fb0cb3b44b72170ad199ad4821a6e357d18d
samettoprak <topraksamet41@gmail.com>
2023-01-03 00:41:04 +0300

    Updated ChannelRestController

--- spring-boot-example/src/main/java/com/samettoprak/springbootexample/REST/ChannelRestController.java
+++ spring-boot-example/src/main/java/com/samettoprak/springbootexample/REST/ChannelRestController.java
@@ -1,14 +1,10 @@
 package com.samettoprak.springbootexample.REST;
 
-import com.samettoprak.springbootexample.DAO.ChannelRepository;
 import com.samettoprak.springbootexample.Entity.Channel;
 import com.samettoprak.springbootexample.Entity.Response;
 import com.samettoprak.springbootexample.Service.ChannelService;
 import com.samettoprak.springbootexample.Service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +12,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/Channels")
 public class ChannelRestController {
-    private ChannelService channelService;
-    private UserService userService;
+    private final ChannelService channelService;
+    private final UserService userService;
+
     public ChannelRestController(ChannelService channelService, UserService userService) {
         this.channelService = channelService;
         this.userService = userService;
@@ -32,18 +29,70 @@ public class ChannelRestController {
             return new Response<>(false, e.getMessage(), null);
         }
     }
+    @GetMapping("/Channel/{channelId}")
+    public Response<Channel> getChannel(@PathVariable String channelId){
+        try {
+            var result = channelService.getChannel(channelId);
+            if(result!=null){
+                return new Response<>(true,null,result);
+            }
+            else return new Response<>(false,"Can't Find Channel",null);
+        }catch (Exception e){
+            return new Response<>(false,e.getMessage(),null);
+        }
+    }
+
+    @PostMapping("/")
+    public Response<Channel> addChannel(@RequestBody Channel channel) {
+        try {
+            return new Response<>(true,null,channelService.addChannel(channel));
+        } catch (Exception e) {
+            return new Response<>(false,e.getMessage(),null);
+        }
+
+    }
+    @PutMapping("/Channel/{channelId}")
+    public Response<Channel> updateChannel(@RequestBody Channel channel,@PathVariable String channelId){
+        try {
+            return new Response<>(true,null,channelService.updateChannel(channel));
+        }catch (Exception e){
+            return new Response<>(false,e.getMessage(),null);
+
+        }
+    }
+    @DeleteMapping("/DeleteChannel/{channelId}")
+    public Response<Boolean> deleteChannel(@PathVariable String channelId){
+        try {
+            return new Response<>(true,null,channelService.deleteChannel(channelId));
+        }catch (Exception e){
+            return new Response<>(false,e.getMessage(),null);
+        }
+    }
 
     @PostMapping("/AddUser/{userId}/ToChannel/{channelId}")
-    public Response<Channel> addUserToChannel(String userId, String channelId) {
+    public Response<Channel> addUserToChannel(@PathVariable String userId,@PathVariable String channelId) {
         try {
             var channel = channelService.findById(channelId);
             var user = userService.findById(userId);
-            if (channel != null && user!=null) {
-                var result = channelService.addUserToChannel(user,channel);
-                return new Response<>(true,null,result);
+            if (channel != null && user != null) {
+                var result = channelService.addUserToChannel(user, channel);
+                return new Response<>(true, null, result);
             } else return new Response<>(false, "Channel Not Found", null);
         } catch (Exception e) {
             return new Response<>(false, e.getMessage(), null);
+        }
+    }
+    @DeleteMapping("/DeleteUser/{userId}/FromChannel/{channelId}")
+    public Response<Channel> deleteUserFromChannel(@PathVariable String userId,@PathVariable String channelId){
+        try {
+            var channel = channelService.deleteUserFromChannel(userId,channelId);
+            if(channel!=null){
+                return new Response<>(true,null,channel);
+            }
+            else return new Response<>(false,"Channel or user not exist",null);
+
+        }catch (Exception e){
+            return new Response<>(false,e.getMessage(),null);
         }
     }
 }
